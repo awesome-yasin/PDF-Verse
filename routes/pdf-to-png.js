450e07aebbe2c62c008f38303408c3e3aa45234e
Md Yasin <yasinthegodofcrypto@gmail.com>
2023-11-26 14:52:48 +0530

    PDF to PNG conversion added

--- routes/pdf-to-png.js
+++ routes/pdf-to-png.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const { pdf } = require('pdf-to-img');
 const JSZip = require('jszip');
 const router = express.Router();
 
@@ -7,6 +8,40 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/pdf-to-png', (req, res) => {
   res.render('pdf-to-png', { title: 'Convert PDF to PNG' });
+});
+
+router.post('/png-convert', upload.single('file'), async (req, res) => {
+  if (!req.file || req.file.mimetype !== 'application/pdf') {
+    res.status(400).send('Please upload a valid PDF file.');
+    return;
+  }
+
+  try {
+    const pdfBuffer = req.file.buffer;
+
+    const doc = await pdf(pdfBuffer);
+    const outputImages = [];
+
+    for await (const page of doc) {
+      outputImages.push(page);
+    }
+
+    const zip = new JSZip();
+
+    outputImages.forEach((img, i) => {
+      zip.file(`output${i}.png`, img);
+    });
+
+    zip.generateAsync({ type: 'nodebuffer' }).then(zipBuffer => {
+      res.setHeader('Content-Disposition', 'attachment; filename=images.zip');
+      res.setHeader('Content-Type', 'application/zip');
+      res.send(zipBuffer);
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('An error occurred while processing the PDF file.');
+  }
 });
 
 module.exports = router;
